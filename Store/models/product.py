b4e461d2d477a8e8fa14e6d9a3f6710146fcc8c6
Hatif'n Party <hatifkhuld90@gmail.com>
2020-11-11 21:01:05 +0530

    ver 1.0

--- Store/models/product.py
+++ Store/models/product.py
@@ -1,4 +1,7 @@
-from django.db import models 
+from django.db import models
 
-class Product(models.Model):
-    
+class Product_s(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.IntegerField(default=0)
+    description = models.CharField(max_length=200, default='')
+    image = models.ImageField(upload_to="products/")
