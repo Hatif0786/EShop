d7488cd365191918680d5144fe5f0268f25aca28
Hatif'n Party <hatifkhuld90@gmail.com>
2020-11-14 21:44:12 +0530

    ver 4.0

--- EShop/urls.py
+++ EShop/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from .settings import MEDIA_ROOT, MEDIA_URL
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("Store.urls"))
-]
+] + static(MEDIA_URL, document_root=MEDIA_ROOT)
